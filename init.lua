92c4bd24f76b5851b17b2d0f3fbbc4460d090907
LordPhyre <60118488+LordPhyre@users.noreply.github.com>
2021-04-17 04:15:36 -0400

    "Minor changes to init.lua"

--- init.lua
+++ init.lua
@@ -36,6 +36,35 @@ minetest.register_tool("galvmith:mithril_sword", {
 })
 
 
+minetest.register_tool("galvmith:dudemanscoolstick", {
+    description = minetest.colorize("#ffffff","Wooden Sword"),
+    inventory_image = "woodstick.png",
+    
+    tool_capabilities = {
+        full_punch_interval = 0.4,
+        max_drop_level = 1,
+        groupcaps = {
+	    cracky = {times = {[1] = 2.25, [2] = 0.55, [3] = 0.35}, uses = 0, maxlevel = 2},
+	    crumbly = {times = {[1] = 0.70, [2] = 0.35, [3] = 0.20}, uses = 0, maxlevel = 2},
+	    choppy = {times = {[1] = 1.75, [2] = 0.45, [3] = 0.45}, uses = 0, maxlevel = 2},
+            snappy = {times = {0.01, 0.07, 0.06}, uses = 0, maxlevel = 2},
+        },
+	groups = {not_in_creative_inventory=1},
+        damage_groups = {fleshy = 5000, burns = 5000},
+    },
+})
+
+
+minetest.register_craft({
+    output = "galvmith:dudemanscoolstick",
+    recipe = {
+        {"default:iron_lump", "default:cobble", "default:wood"},
+        {"default:iron_lump", "default:cobble", "default:wood"},
+        {"default:iron_lump", "default:cobble", "default:wood"},
+    }
+})
+
+
     minetest.register_tool("galvmith:mithril_pickaxe", {
         description = minetest.colorize(blu,"Galvanized Mithril Pickaxe"),
         inventory_image = "galvmith_mithril_pickaxe.png",
